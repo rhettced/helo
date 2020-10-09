9662a60ebb3ec6c9aa89faf687bb0d5b4537b0da
Mac <rhettced@gmail.com>
2020-10-09 16:21:06 -0600

    changing table

--- src/Components/Dashboard/DashboardDisplay.js
+++ src/Components/Dashboard/DashboardDisplay.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react';
 import './DashboardDisplay.css';
+import {Link} from 'react-router-dom';
 
 class DashboardDisplay extends Component{
 
 render(){
     return(
-        <section>
-            <h2>{this.props.title}</h2>
-            <div className='post-by'>
-                <p>by: {this.props.author} </p>
-                <img src={this.props.img}/>
-            </div>
-        </section>
+        <Link to={`/post/`}>
+            <section>
+                <h2>{this.props.title}</h2>
+                <div className='post-by'>
+                    <p>by: {this.props.author} </p>
+                    <img src={this.props.img}/>
+                </div>
+            </section>
+        </Link>
     );
 }
 
