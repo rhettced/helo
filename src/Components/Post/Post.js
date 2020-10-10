31b7e070450ecb3ab230ab45beec680cf27840ed
Mac <rhettced@gmail.com>
2020-10-09 19:00:41 -0600

    worked on posts

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import './Post.css';
 
 class Post extends Component {
     constructor(){
         super();
         this.state = {
             title: '',
-            img: '',
+            img: 'https://storyantics.com/images/images/en/imageNoImageSmall.gif',
             content: '',
             author:'',
             authorPicture:''
@@ -13,7 +14,19 @@ class Post extends Component {
     }
     render(){
         return(
-            <div>Post</div>
+            <div className='single-post'>
+                <div className='post-header'>
+                    <h1>Title</h1>
+                    <div className='author-display'>
+                        <p>by: </p>
+                        <img src={this.state.img}/>
+                    </div>  
+                </div>
+                <div className='pic-post'>
+                        <img src={this.state.img}/>
+                        <p>Here goes all the post content</p>
+                </div>
+            </div>
         );
     }
 }
